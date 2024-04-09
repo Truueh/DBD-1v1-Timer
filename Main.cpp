8e9d7e3452460a996122c5620335bfdee9af834e
dokka <zoritn@gmail.com>
2024-04-09 17:44:13 +0300

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include <Windows.h>
+#include <d2d1.h>
+#include <string>
+#include <wchar.h>
+#include <thread>
+#include <dwrite.h>
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+
+}
