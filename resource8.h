2a4202905b52b38352069397fe56ed1274e55ec9
Truueh <zoritn@gmail.com>
2024-04-19 03:26:42 +0300

    Added an icon for the app.

---
+++ resource8.h
@@ -0,0 +1,18 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by DBD 1v1 Timer1.rc
+//
+#define IDB_PNG1                        101
+#define IDB_BITMAP1                     103
+#define IDI_ICON1                       104
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
