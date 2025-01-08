adbc496547b016b8157021ed750db133280bee70
Truueh <zoritn@gmail.com>
2025-01-08 18:23:25 +0200

    Removed the recently added support for structuring windows dynamically.

--- WindowStructure.cpp
+++
@@ -1,7 +0,0 @@
-#include "WindowStructure.h"
-
-WindowStructure::WindowStructure(int rows, int width, int height)
-{
-	for (int i = 0; i < rows; i++)
-		Rows[i] = Row(rows, width, height / rows);
-}
